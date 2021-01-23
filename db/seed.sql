44431ec73f9581de617ff678abb6ac8080e68964
Efrain Gonzalez <jesusgonzalez0797@gmail.com>
2021-01-23 18:47:59 -0500

    add sql schema and seed files

---
+++ db/seed.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers(burger_name, devoured) VALUES ("burger 1", false);
+INSERT INTO burgers(burger_name, devoured) VALUES ("burger 2", false);
+INSERT INTO burgers(burger_name, devoured) VALUES ("burger 3", true);
