5df659a86b09f859139adf1f0626886d7305adf8
Efrain Gonzalez <jesusgonzalez0797@gmail.com>
2021-01-26 15:33:32 -0500

    set up server.js to run burger routes

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
+// set up express app
 const express = require('express');
-
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+// set up express handlebars 
+const exphbs = require('express-handlebars');
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// import burger controller
+const routes = require('./controllers/burgers_controller.js');
+app.use(routes);
+
+// begin server
 const PORT = process.env.PORT || 3000;
-
-
 app.listen(PORT, (err) => {
     if(err) console.error(err);
     console.log(`now listening on port ${PORT}`);
