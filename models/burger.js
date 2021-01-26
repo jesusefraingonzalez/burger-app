ea7cb8b3bcb05d4abd14bb0fbda25f1f61bee2f2
Efrain Gonzalez <jesusgonzalez0797@gmail.com>
2021-01-26 14:15:12 -0500

    add burger function abstractions

--- models/burger.js
+++ models/burger.js
@@ -1,1 +1,15 @@
 const {selectAll , insertOne, updateOne} = require('../config/orm.js');
+
+let burgers = {
+   selectAll: () => {
+       selectAll('burgers');
+   },
+   insertOne: (burgerName, isDevoured) => {
+       insertOne('burgers' , burgerName, isDevoured);
+   },
+   updateOne: (burgerName, isDevoureds) => {
+       updateOne('burgers', burgerName, isDevoured);
+   }
+}
+
+module.exports = burgers;
