aca803991c2127d89a310bcddec74095eb2e2aae
Efrain Gonzalez <jesusgonzalez0797@gmail.com>
2021-01-26 15:33:06 -0500

    add burger routes to controller file

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,17 +1,30 @@
 const express = require('express');
-const {selectAll, insertOne, updateOne} = require('../models/burger.js');
+const burger = require('../models/burger.js');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-
+    burger.all((data) => {
+        let object = {
+            burgers: data
+        };
+        res.render('index', object);
+    });
 });
 
 router.post('/api/burgers', (req, res) => {
-
+    burger.create(["burger_name", "devoured"], [req.body.name, req.body.devoured], (result) => {
+        res.json({ id: result.insertId });
+    });
 });
 
-router.put('/api/burgers', (req, res) => {
+router.put('/api/burgers/:id', (req, res) => {
+    let condition = `id = ${req.params.id}`;
+    console.log(`condition: ${condition}`);
 
+    burger.update({devoured: req.body.devoured}, condition, (result) => {
+        if(result.changedRows == 0) return res.status(404).end();
+        else return res.status(200).end();
+    });
 });
 
 module.exports = router;
