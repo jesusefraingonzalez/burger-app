d32d2903f3be253837df514f20587648ec9fc511
Efrain Gonzalez <jesusgonzalez0797@gmail.com>
2021-01-26 13:45:34 -0500

    add updateOne() and update query strings

--- config/orm.js
+++ config/orm.js
@@ -2,25 +2,29 @@ const connection = require('./connection.js');
 
 let orm = {
     selectAll: (dbTable) => {
-        let queryString = "SELECT * FROM " + dbTable + ";";
-        connection.query(queryString, (err, result) => {
+        let queryString = "SELECT * FROM ??;";
+        connection.query(queryString, dbTable, (err, result) => {
             if(err) throw err;
             console.table(result);
             return result;
         });
     },
 
-    insertOne: (burgerName, isDevoured, dbTable) => {
-        let queryString = "INSERT INTO " + dbTable + '(burger_name, devoured) VALUES ("' + burgerName + '", ' + isDevoured + ");";
-        connection.query(queryString, (err, res) => {
+    insertOne: (dbTable, burgerName, isDevoured) => {
+        let queryString = 'INSERT INTO ?? (burger_name, devoured) VALUES (?, ?)';
+        connection.query(queryString, [dbTable, burgerName , isDevoured] , (err, res) => {
             if(err) throw err;
             return res;
         });
 
     },
 
-    updateOne: () => {
-
+    updateOne: (dbTable, burgerName, isDevoured) => {
+        let queryString = "UPDATE ?? SET devoured = ? WHERE burger_name = ?";
+        connection.query(queryString, [dbTable, isDevoured, burgerName], (err, res) => {
+            if(err) throw err;
+            return res;
+        })
     }
 }
 
