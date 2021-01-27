e9ad7be3cbcdfc2ae9fd3265e6c9d7ed7779a5b1
Efrain Gonzalez <jesusgonzalez0797@gmail.com>
2021-01-26 23:51:50 -0500

    add jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,21 @@
 const mysql = require('mysql');
 
-let connection = mysql.createConnection({
-    host: "td5l74lo6615qq42.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    port: 3306,
-    user: "o4kfednej7qkiorx",
-    password: "ux5p6g7ho971x5ob",
-    database: "jj3jxa7i6t070btk"
-});
+let connection;
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+    connection = mysql.createConnection({
+        host: "td5l74lo6615qq42.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+        port: 3306,
+        user: "o4kfednej7qkiorx",
+        password: "ux5p6g7ho971x5ob",
+        database: "jj3jxa7i6t07k0btk"
+    });
+}
 
 connection.connect((err) => {
-    if(err) throw err;
+    if (err) throw err;
     console.log("Now connected to mysql");
 });
 
