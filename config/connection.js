4a0a07e28bc6aaf4f779e73e99087df608d5fa3e
Efrain Gonzalez <jesusgonzalez0797@gmail.com>
2021-01-26 23:37:35 -0500

    add jawsDB

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 let connection = mysql.createConnection({
     host: "td5l74lo6615qq42.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     port: 3306,
-    user: "	o4kfednej7qkiorx",
+    user: "o4kfednej7qkiorx",
     password: "ux5p6g7ho971x5ob",
     database: "jj3jxa7i6t070btk"
 });
